745a49442b212b6b3916b988d12ca725538cd4d4
Gabriel <gabriel59kg@gmail.com>
2019-04-19 12:52:58 -0400

    update tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,30 @@
 """tests/test_basic.py"""
+import pathlib
+import importlib
+from setuptools import find_packages
 
+import pytest
+
+ROOT = (pathlib.Path(__file__) / ".." / "..").resolve()
 
 def test_for_fire():
-    import blue_chip
+    all_modules = find_packages(exclude=["tests"])
+    on_fire = {}
 
-    print(blue_chip.__doc__)
+    for module in all_modules:
+        try:
+            importlib.import_module(module)
+            print(f"✔️  {module}")
+        except ImportError as import_error:
+            on_fire[module] = import_error
+            print(f"❌  {module}")
+        except Exception as error:
+            on_fire[module] = error
+            print(f"❌  {module}")
+    
+    if on_fire:
+        raise ImportError(", ".join(on_fire.keys()))
+
+if __name__ == "__main__":
+    print(ROOT)
+    pytest.main(args=[__file__, "-v"])
