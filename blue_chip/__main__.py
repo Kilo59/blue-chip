b0403f2aab057fe73be3a3f5b09209190d23ebc0
Gabriel <gabriel59kg@gmail.com>
2020-02-16 15:15:21 -0500

    move to poetry (#27)
    * get version from setuptools (pkg_resources)
    
    * add poetry (#26)
    
    * add lgtm badge
    
    * poetry project setup
    
    * fix topic classifier
    
    * remove flit from Pipfile and add warning
    
    warn about moving to Poetry
    
    * use poetry for travis build
    
    * test get_version() doesn't catch on fire
    
    * 0.0.7 version for pre-release

--- blue_chip/__main__.py
+++ blue_chip/__main__.py
@@ -2,11 +2,33 @@
 blue_chip.__main__.py
 ~~~~~~~~~~~~~~~~~~~~~
 """
+import pkg_resources
 from invoke import Collection, Program  # pylint: disable=import-error
 
-from blue_chip import __version__, tasks
+import blue_chip.tasks
+
+PACKAGE_NAME = "blue-chip"
+
+
+def get_version(pkg_name=PACKAGE_NAME):
+    """
+    Get the package version.
+
+    https://github.com/python-poetry/poetry/issues/144#issuecomment-559793020
+    """
+    try:
+        distribution = pkg_resources.get_distribution(pkg_name)
+    except pkg_resources.DistributionNotFound:
+        return "dev"
+    else:
+        return distribution.version
+
+
+PKG_VERSION = get_version()
 
 # pylint: disable=invalid-name
 program = Program(
-    namespace=Collection.from_module(tasks), name="blue-chip", version=__version__
+    namespace=Collection.from_module(blue_chip.tasks),
+    name=PACKAGE_NAME,
+    version=PKG_VERSION,
 )
