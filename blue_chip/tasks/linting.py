720283c6857aa3022868b6a55412cdf852de36f7
Gabriel <gabriel59kg@gmail.com>
2021-04-06 20:24:15 -0400

    cfg-lint --force

--- blue_chip/tasks/linting.py
+++ blue_chip/tasks/linting.py
@@ -18,21 +18,20 @@ STAR_SEP = "*" * get_terminal_size().columns
 
 
 @task
-def cfg_lint(ctx):  # pylint:disable=unused-argument
+def cfg_lint(ctx, force=False):  # pylint:disable=unused-argument
     """Configure prospector profiles."""
-    if constants.BC_LINTRC_PATH.exists():
-        return
-    lintrc_path = pathlib.Path(constants.BC_LINTRC_PATH)
-    lintrc_path.mkdir(exist_ok=False)
-    for profile_name, profile_content in config.data.LINT_DATA.items():
-        profile_path = lintrc_path / profile_name
-        print(f"  Initializing {profile_name} ...")
-        with open(profile_path, mode="w") as f_out:
-            f_out.write(profile_content)
-    print(
-        "Lint configuration profiles created at:\n",
-        f"\t{constants.BC_LINTRC_PATH}\n{STAR_SEP}",
-    )
+    if force or not constants.BC_LINTRC_PATH.exists():
+        lintrc_path = pathlib.Path(constants.BC_LINTRC_PATH)
+        lintrc_path.mkdir(exist_ok=True)
+        for profile_name, profile_content in config.data.LINT_DATA.items():
+            profile_path = lintrc_path / profile_name
+            print(f"  Initializing {profile_name} ...")
+            with open(profile_path, mode="w") as f_out:
+                f_out.write(profile_content)
+        print(
+            "Lint configuration profiles created at:\n",
+            f"\t{constants.BC_LINTRC_PATH}\n{STAR_SEP}",
+        )
 
 
 @task(pre=[cfg_lint])
