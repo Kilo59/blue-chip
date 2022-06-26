bb86ff70b68b39ce6db394d346178ad5dea1aaf0
Gabriel Gore <gabriel59kg@gmail.com>
2022-06-25 22:30:05 -0400

    add `cfg-lint --force` command

--- blue_chip/tasks/linting.py
+++ blue_chip/tasks/linting.py
@@ -9,7 +9,6 @@ from invoke import task
 
 from blue_chip import config, constants
 
-# pylint:disable=protected-access
 
 __all__ = ["cfg_lint", "lint"]
 
@@ -18,16 +17,16 @@ STAR_SEP = "*" * get_terminal_size().columns
 
 
 @task
-def cfg_lint(ctx):  # pylint:disable=unused-argument
+def cfg_lint(ctx, force=False):  # pylint:disable=unused-argument
     """Configure prospector profiles."""
-    if constants.BC_LINTRC_PATH.exists():
+    if constants.BC_LINTRC_PATH.exists() and not force:
         return
     lintrc_path = pathlib.Path(constants.BC_LINTRC_PATH)
-    lintrc_path.mkdir(exist_ok=False)
+    lintrc_path.mkdir(exist_ok=True)
     for profile_name, profile_content in config.data.LINT_DATA.items():
         profile_path = lintrc_path / profile_name
         print(f"  Initializing {profile_name} ...")
-        with open(profile_path, mode="w") as f_out:
+        with open(profile_path, mode="w", encoding="utf-8") as f_out:
             f_out.write(profile_content)
     print(
         "Lint configuration profiles created at:\n",
